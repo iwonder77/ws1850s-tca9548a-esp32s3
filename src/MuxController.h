6953ae791a2a1c77512b79e0365ba66f6f7887e8
Isai Sanchez <isaisanchez@20TPI14365-MacAir.local>
2026-03-03 13:30:59 -0700

    modify file to use Config constants

--- src/MuxController.h
+++ src/MuxController.h
@@ -5,23 +5,23 @@
  * Centralized logic for switching channels on I2C mux
  */
 #include <Arduino.h>
+#include <Wire.h>
 
 #include "Config.h"
-#include "Wire.h"
 
 class MuxController {
 public:
-  static void selectChannel(uint8_t muxAddress, uint8_t channel) {
+  static void selectChannel(uint8_t channel) {
     if (channel > 7)
       return;
-    Wire.beginTransmission(muxAddress);
+    Wire.beginTransmission(config::MUX_ADDR);
     Wire.write(1 << channel);
     Wire.endTransmission();
 
     delay(config::CHANNEL_SWITCH_SETTLE_MS);
   }
-  static void disableChannel(uint8_t muxAddress) {
-    Wire.beginTransmission(muxAddress);
+  static void disableChannel() {
+    Wire.beginTransmission(config::MUX_ADDR);
     Wire.write(0);
     Wire.endTransmission();
 
